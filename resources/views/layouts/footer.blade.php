35f47faee9bf708008a72db284ce200ad88de339
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-04-10 19:59:32 +0600

    First Commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    
+    <center>
+      <strong>Copyright &copy; {{ date('Y') }} <a href="#">BoboIT</a>.</strong> All rights reserved.
+    </center>
+
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends ('master')
+
+@section('page-title')
+	Dashboard
+@endsection
+
+@section('content')
+	Welcome to Inventory Management System!
+@endsection
